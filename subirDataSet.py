1b6f66fb65c661c73e2124aa02599d3efd2c37a6
Darcher08 <148842609+Darcher08@users.noreply.github.com>
2023-12-02 14:39:34 -0600

    Update subirDataSet.py

--- subirDataSet.py
+++ subirDataSet.py
@@ -1,17 +1,24 @@
 import couchdb
 import json
+""" 
+En caso de no tener las librerias utilizar las siguientes instalaciones: 
+pip install CouchDB
+pip install json
+"""
+
 
 # Conectar al servidor CouchDB
+#para ello se utiliza la direccion del servidor junto con el usuario y contraseña
 #http://usuario:contraseña
 couch = couchdb.Server('http://admin:admin@localhost:5984/')
 
-# Seleccionar la base de datos
+# Se debe poner el nombre de la BD que se creo en el cliente
 db = couch['albums']
 
-# Leer el archivo JSON
+# Leemo el archivo Json
 with open('dataset.json', 'r') as f:
     data = json.load(f)
 
-# Guardar cada documento en la base de datos
+# Se guarda cada uno de los documentos en la BD
 for doc in data:
     db.save(doc)
